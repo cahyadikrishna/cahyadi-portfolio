7b5f20d5a2a5e7688848415c2a3668523d35a63d
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2024-05-10 18:49:30 +0800

    feat: implement tech stack section

---
+++ src/modules/home/entities/home.entity.ts
@@ -0,0 +1,42 @@
+export const TECH_STACK_DATA = [
+  {
+    name: 'HTML',
+    path: '/src/assets/icons/html.png',
+  },
+  {
+    name: 'CSS',
+    path: '/src/assets/icons/css.png',
+  },
+  {
+    name: 'SCSS',
+    path: '/src/assets/icons/sass.png',
+  },
+  {
+    name: 'Javascript',
+    path: '/src/assets/icons/javascript.png',
+  },
+  {
+    name: 'Typescript',
+    path: '/src/assets/icons/typescript.png',
+  },
+  {
+    name: 'Github',
+    path: '/src/assets/icons/github.png',
+  },
+  {
+    name: 'Vue.js',
+    path: '/src/assets/icons/vuejs.png',
+  },
+  {
+    name: 'Nuxt',
+    path: '/src/assets/icons/nuxt.png',
+  },
+  {
+    name: 'React.js',
+    path: '/src/assets/icons/react.png',
+  },
+  {
+    name: 'Express.js',
+    path: '/src/assets/icons/express-js.png',
+  },
+];
