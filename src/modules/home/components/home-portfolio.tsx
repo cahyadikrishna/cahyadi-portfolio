a408c4eed9f2530bad4d1cbb79392f88378f3842
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2024-05-20 11:21:31 +0800

    refactor: implement date format and responsive card portfolio

--- src/modules/home/components/home-portfolio.tsx
+++ src/modules/home/components/home-portfolio.tsx
@@ -17,6 +17,17 @@ export default function HomePortfolio() {
     getPortfolio();
   }, []);
 
+  function getPortfolioYear(startDate: string, endDate: string | undefined) {
+    const portfolioStartYear = new Date(startDate).getFullYear();
+
+    if (endDate === undefined) {
+      return `${portfolioStartYear} - Present`;
+    }
+
+    const portfolioEndYear = new Date(endDate).getFullYear();
+    return `${portfolioStartYear} - ${portfolioEndYear}`;
+  }
+
   return (
     <section className="max-w-[1200px] mx-auto px-[20px] mt-[50px]">
       <TabGroup>
@@ -40,23 +51,30 @@ export default function HomePortfolio() {
               {portfolio.contents!.map((content, i) => (
                 <div key={i} className="bg-gray-main rounded-[30px] p-[30px]">
                   <div className="flex flex-col gap-[10px]">
-                    <div className="flex items-start justify-between">
+                    <div className="flex flex-col gap-[8px] items-start justify-between lg:gap-[20px] lg:flex-row">
                       <h2 className="font-semibold text-[20px] max-w-[330px]">
                         {content.title}
                       </h2>
 
                       <div className="bg-primary text-white text-[12px] font-semibold px-[12px] py-[4px] rounded-[10px]">
-                        <span>2020 - Present</span>
+                        <span>
+                          {getPortfolioYear(
+                            String(content.startDate),
+                            content.endDate
+                          )}
+                        </span>
                       </div>
                     </div>
 
-                    <span className="font-semibold text-primary">
-                      Core Team
+                    <span className="text-[12px] font-semibold text-primary lg:text-[16px]">
+                      {content.role}
                     </span>
 
                     <hr className="border border-b-2 rounded-full" />
 
-                    <p className="text-dark-secondary">{content.description}</p>
+                    <p className="text-[14px] text-dark-secondary md:text-[16px]">
+                      {content.description}
+                    </p>
                   </div>
                 </div>
               ))}
